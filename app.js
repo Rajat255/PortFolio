89098c4bffccbc2906a4bb62caad2df38f6b0ec2
Rajat255 <rajatkhirid@gmail.com>
2023-08-16 17:54:07 +0530

    Adding basic SKeleton

---
+++ app.js
@@ -0,0 +1,10 @@
+VANTA.NET({
+    el: "#aboutme",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    scaleMobile: 1.00
+  })
